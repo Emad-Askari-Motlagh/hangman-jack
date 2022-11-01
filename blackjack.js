733e9a13cd28a6f881ce7465039c3b52ff4990a6
emadi65 <emad.askari@gmail.com>
2022-11-01 15:57:08 +0100

    First commit

--- blackjack.js
+++ blackjack.js
@@ -1,148 +1,181 @@
-
 var dealerSum = 0;
 var yourSum = 0;
-
 var dealerAceCount = 0;
-var yourAceCount = 0; 
-
+var yourAceCount = 0;
 var hidden;
 var deck;
+const initiatedCards = new Array(1).fill("card");
+var canHit = true;
 
-var canHit = true; //allows the player (you) to draw while yourSum <= 21
-
-window.onload = function() {
-    buildDeck();
-    shuffleDeck();
-    startGame();
-}
+window.onload = function () {
+  buildDeck();
+  shuffleDeck();
+  startGame();
+};
 
 function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"];
-    deck = [];
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  let types = ["C", "D", "H", "S"];
+  deck = [];
 
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + types[i]); //A-C -> K-C, A-D -> K-D
-        }
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      deck.push(values[j] + "-" + types[i]); //A-C -> K-C, A-D -> K-D
     }
-    // console.log(deck);
+  }
+  // console.log(deck);
 }
 
 function shuffleDeck() {
-    for (let i = 0; i < deck.length; i++) {
-        let j = Math.floor(Math.random() * deck.length); // (0-1) * 52 => (0-51.9999)
-        let temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-    console.log(deck);
+  const cards = new Array(deck.length).fill("card");
+  deck.forEach((element, i) => {
+    let j = Math.floor(Math.random() * deck.length); // (0-1) * 52 => (0-51.9999)
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  });
 }
+function dealerRun() {
+  const stayButton = document.getElementById("stay");
+  hidden = deck.pop();
+  dealerSum += getValue(hidden);
+  dealerAceCount += checkAce(hidden);
 
+  stayButton.setAttribute("disabled", true);
+
+  while (dealerSum < 17) {
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    dealerSum += getValue(card);
+    dealerAceCount += checkAce(card);
+    document
+      .getElementById("dealer-cards")
+      .insertAdjacentElement("afterbegin", cardImg);
+  }
+}
 function startGame() {
-    hidden = deck.pop();
-    dealerSum += getValue(hidden);
-    dealerAceCount += checkAce(hidden);
-    // console.log(hidden);
-    // console.log(dealerSum);
-    while (dealerSum < 17) {
-        //<img src="./cards/4-C.png">
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        dealerSum += getValue(card);
-        dealerAceCount += checkAce(card);
-        document.getElementById("dealer-cards").append(cardImg);
-    }
-    console.log(dealerSum);
-
-    for (let i = 0; i < 2; i++) {
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        yourSum += getValue(card);
-        yourAceCount += checkAce(card);
-        document.getElementById("your-cards").append(cardImg);
-    }
-
-    console.log(yourSum);
-    document.getElementById("hit").addEventListener("click", hit);
-    document.getElementById("stay").addEventListener("click", stay);
-
-}
-
-function hit() {
-    if (!canHit) {
-        return;
-    }
-
+  dealerRun();
+  initiatedCards.map((res, i) => {
     let cardImg = document.createElement("img");
     let card = deck.pop();
     cardImg.src = "./cards/" + card + ".png";
     yourSum += getValue(card);
     yourAceCount += checkAce(card);
     document.getElementById("your-cards").append(cardImg);
+  });
 
-    if (reduceAce(yourSum, yourAceCount) > 21) { //A, J, 8 -> 1 + 10 + 8
-        canHit = false;
-    }
+  document.getElementById("hit").addEventListener("click", hit);
+  document.getElementById("stay").addEventListener("click", stay);
+}
 
+function hit() {
+  if (!canHit) {
+    return;
+  }
+  const stayButton = document.getElementById("stay");
+  stayButton.removeAttribute("disabled");
+  let cardImg = document.createElement("img");
+  let card = deck.pop();
+  cardImg.src = "./cards/" + card + ".png";
+  yourSum += getValue(card);
+  yourAceCount += checkAce(card);
+  document.getElementById("your-cards").append(cardImg);
+
+  if (reduceAce(yourSum, yourAceCount) > 21) {
+    //A, J, 8 -> 1 + 10 + 8
+    canHit = false;
+  }
 }
 
 function stay() {
-    dealerSum = reduceAce(dealerSum, dealerAceCount);
-    yourSum = reduceAce(yourSum, yourAceCount);
+  const confirm = document.querySelector(".confirm-button");
+  confirm.textContent = "ok";
+  confirm.style.backgroundColor = "#282828";
+  dealerSum = reduceAce(dealerSum, dealerAceCount);
+  yourSum = reduceAce(yourSum, yourAceCount);
 
-    canHit = false;
-    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+  canHit = false;
+  document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+  const resultButton = document.getElementById("results");
+  let showModal = false;
+  let message = "";
 
-    let message = "";
-    if (yourSum > 21) {
-        message = "You Lose!";
-    }
-    else if (dealerSum > 21) {
-        message = "You win!";
-    }
-    //both you and dealer <= 21
-    else if (yourSum == dealerSum) {
-        message = "Tie!";
-    }
-    else if (yourSum > dealerSum) {
-        message = "You Win!";
-    }
-    else if (yourSum < dealerSum) {
-        message = "You Lose!";
-    }
+  if (yourSum > 21) {
+    message = "You Lose!";
+    showModal = true;
+  } else if (dealerSum > 21) {
+    message = "You win!";
+    showModal = true;
+  }
+  //both you and dealer <= 21
+  else if (yourSum == dealerSum) {
+    message = "Tie!";
+    showModal = true;
+  } else if (yourSum > dealerSum) {
+    message = "You Win!";
+    showModal = true;
+  } else if (yourSum < dealerSum) {
+    message = "You Lose!";
+    showModal = true;
+  }
+  if (showModal) {
+    resultButton.classList.add("results-show");
+    confirm.textContent = "Play Again";
+  }
+  confirm.addEventListener("click", () => {
+    resultButton.classList.remove("results-show");
+    confirm.textContent = null;
 
-    document.getElementById("dealer-sum").innerText = dealerSum;
-    document.getElementById("your-sum").innerText = yourSum;
-    document.getElementById("results").innerText = message;
+    //REload the page to play again
+    location.reload();
+  });
+  document.getElementById("dealer-sum").innerText = dealerSum;
+  document.getElementById("your-sum").innerText = yourSum;
+  document.getElementById(
+    "results-p"
+  ).innerHTML = `<div><div>${message}</div><div>${yourSum} vs ${dealerSum}</div> </div>`;
 }
 
 function getValue(card) {
-    let data = card.split("-"); // "4-C" -> ["4", "C"]
-    let value = data[0];
-
-    if (isNaN(value)) { //A J Q K
-        if (value == "A") {
-            return 11;
-        }
-        return 10;
+  let data = card.split("-");
+  let value = data[0];
+  let types = ["C", "D", "H", "S"];
+  if (isNaN(value)) {
+    //A J Q K
+    if (value == "A") {
+      return 11;
     }
-    return parseInt(value);
+    return 10;
+  }
+  return parseInt(value);
 }
 
 function checkAce(card) {
-    if (card[0] == "A") {
-        return 1;
-    }
-    return 0;
+  if (card[0] == "A") {
+    return 1;
+  }
+  return 0;
 }
 
 function reduceAce(playerSum, playerAceCount) {
-    while (playerSum > 21 && playerAceCount > 0) {
-        playerSum -= 10;
-        playerAceCount -= 1;
-    }
-    return playerSum;
+  while (playerSum > 21 && playerAceCount > 0) {
+    playerSum -= 10;
+    playerAceCount -= 1;
+  }
+  return playerSum;
 }
---
+++ hangman/index.js
@@ -0,0 +1,164 @@
+var mistkaesCount = 0;
+let guesses = [];
+let currentGuess = "";
+const winWord = "ok";
+const classes = null;
+const hangClasses = ["scaffold", "head", "body", "arms", "legs"];
+let guessWord = new Array(winWord.length).fill(undefined);
+// document.querySelector("figure").classList.add("head");
+// document.querySelector("figure").classList.add("body");
+// document.querySelector("figure").classList.add("arms");
+// document.querySelector("figure").classList.add("legs");
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+window.onload = function () {
+  start();
+  ClickAlphabet();
+};
+
+function ClickAlphabet() {
+  const alphabetItems = document.querySelectorAll("#alphabet-item");
+  const ul = document.getElementById("alphabet");
+  const childrens = alphabetItems;
+  if (childrens) {
+    childrens.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        console.log(e.target.textContent);
+        currentGuess = e.target.textContent;
+        checkTheChar();
+      });
+    });
+  }
+}
+
+function executePLayAgainButton() {
+  const playAgainButton = document.getElementById("play-again");
+  playAgainButton.style.display = "block";
+  playAgainButton.textContent = "Play Again";
+
+  playAgainButton.addEventListener("click", () => {
+    location.reload();
+  });
+}
+
+function gameOver() {
+  const gameOverItem = document.getElementById("game-over");
+  gameOverItem.classList.toggle("gameover");
+  const span = document.createElement("span");
+  span.textContent = "Game Over";
+  gameOverItem.insertAdjacentElement("afterbegin", span);
+  executePLayAgainButton();
+}
+function drawHangPieces() {
+  const head = document.getElementById("head");
+  const body = document.getElementById("body");
+  const arms = document.getElementById("arms");
+  const legs = document.getElementById("legs");
+  const scaffold = document.getElementById("scaffold");
+  switch (mistkaesCount) {
+    case 1:
+      console.log("1 mistake");
+      scaffold.style.display = "flex";
+      scaffold.style.transition = "all 3s ease-in-out";
+      return;
+    case 2:
+      console.log("2 mistake");
+      head.style.display = "flex";
+      arms.style.transition = "all 2s ease-in";
+      return;
+    case 3:
+      console.log("3 mistake");
+      body.style.display = "flex";
+      arms.style.transition = "all 2s ease-in";
+      return;
+    case 4:
+      console.log("4 mistake");
+      arms.style.display = "flex";
+      return;
+    case 5:
+      console.log("4 mistake");
+      legs.style.display = "flex";
+      return;
+    case 6:
+      gameOver();
+      return;
+  }
+}
+
+function checkTheChar() {
+  const container = document.getElementById("guesses-show");
+  let showCaseItem = document.querySelector(".guess-item");
+  let found;
+
+  for (let i = 0; i < winWord.length; i++) {
+    if (winWord[i] === currentGuess) {
+      container.children[i].textContent = currentGuess;
+      guessWord.splice(i, 1, currentGuess);
+      found = true;
+    }
+  }
+  if (!found) {
+    mistkaesCount++;
+    drawHangPieces();
+  }
+  if (JSON.stringify(guessWord) == JSON.stringify(Array.from(winWord))) {
+    const winCard = document.getElementById("win-card");
+    const winCardText = document.getElementById("win-card-text");
+    winCard.style.display = "flex";
+    winCard.classList.toggle("win-card-show");
+    winCardText.textContent = "Congratulation! You Won";
+    executePLayAgainButton();
+  }
+}
+
+//Satart the game
+function start() {
+  mistkaesCount = 0;
+  const container = document.getElementById("guesses-show");
+
+  //create keyboard
+  const ul = document.getElementById("alphabet");
+  for (let i = 0; i < alphabet.length; i++) {
+    const liItem = document.createElement("li");
+    liItem.id = "alphabet-item";
+    liItem.textContent = alphabet[i];
+    ul.insertAdjacentElement("afterbegin", liItem);
+  }
+
+  //Show guesses
+
+  Array.from(winWord).forEach((element) => {
+    const el = document.createElement("span");
+    el.style.margin = "10px";
+    el.textContent = "_";
+    container.appendChild(el);
+    el.classList.add("guess-item");
+  });
+}
